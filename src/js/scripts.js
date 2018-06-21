05b298ad66c911550061688434ec09f7e1d7db23
Tyler Gray <tgray.it@gmail.com>
2018-06-20 21:22:11 -0500

    Nav Fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,17 +16,7 @@ function removeClass(ele,cls) {
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
-var feed = new Instafeed({
-  get: 'user',
-  userId: '2923101612',
-  clientId: '86cf11df1958430484c96f4b0e823b73',
-  accessToken: '2923101612.1677ed0.7948f055fe3e4e0cb89dc19982633976',
-  template: '<a class="block-instagram" style="background-image: url({{image}});" href="{{link}}"></a>',
-  sortBy: 'most-recent',
-  limit: '8',
-  resolution: 'standard_resolution'
-});
-feed.run();
+
 
 window.onload = function () {
 	// good to go!
@@ -44,7 +34,7 @@ window.onload = function () {
     sr.reveal('.block-title', {origin: 'bottom', scale: 1, duration: 600});
     sr.reveal('.block-image', {origin: 'bottom', scale: 1, duration: 600 });
 
-    sr.reveal('.block-instagram', {origin: 'bottom', scale: 1, duration: 600 }, 200);
+
     var menuButton = document.getElementById('navButton');
     var mobileNav = document.getElementById('mobile-nav');
     menuButton.addEventListener('click', function (e) {
