ede1fd5ba3be5b8305b666b520223c9b3d989167
Tyler Gray <tgray.it@gmail.com>
2018-06-20 21:46:19 -0500

    Nav Fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,8 +13,13 @@ function removeClass(ele,cls) {
   }
 }   
 
-function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
 
 
@@ -44,7 +49,7 @@ window.onload = function () {
       if (hasClass(mobileNav, 'load')) {
         removeClass(mobileNav, 'load');
         addClass(mobileNav, 'exit');
-        sleep(1200).then(() => {
+        sleep(200).then(() => {
           addClass(mobileNav, 'hidden');
         })
         
