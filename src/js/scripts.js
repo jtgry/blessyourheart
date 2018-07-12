a0242e24af6c7634758e4fce73b37006cd2787bd
Tyler Gray <tgray.it@gmail.com>
2018-07-11 23:06:08 -0500

    Instafeed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -28,7 +28,7 @@ var feed = new Instafeed({
   userId: '2923101612',
   clientId: '86cf11df1958430484c96f4b0e823b73',
   accessToken: '2923101612.1677ed0.7948f055fe3e4e0cb89dc19982633976',
-  template: '<a class="block-instagram" style="background-image: url({{image}});" href="{{link}}"></a>',
+  template: '<a class="block-instagram" href="{{link}}"><img src="{{image}}"></a>',
   sortBy: 'most-recent',
   limit: '8',
   resolution: 'standard_resolution'
